51677379882f3884dfc934c7246c8990ac75db65
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-04-12 08:53:09 +0900

    Update: 04.12 day update
    apex chart 사용중
    line, area, bar chart 작성중

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mime = require('mime');
+
 const fs = require('fs');
 const port = 8080;
 
@@ -55,4 +57,52 @@ app.get('/dynamic-page', (req, res) => {
 
 app.get('/test', function (req, res) {
     res.send('the REST endpoint test run!');
+});
+
+app.get('/apex/1', (req, res) => {
+    html = fs.readFileSync('views/apexbasic.html');
+    res.writeHead(200);
+    // res.render('index');
+    res.write(html);
+    res.end();
+});
+
+// app.get('/apex/line/:id', (req, res) => {
+//     const id = req.params.id;
+//     // console.log(`id: ${id}`);
+
+//     let filePath = `views/apex/line/apex_line_${id}.html`;
+//     console.log(`filePath: ${filePath}`);
+
+//     // html = fs.readFileSync(`views/apex/line/apex_line_${id}.html`);
+    
+//     res.sendFile(path.join(__dirname, `views/apex/line/apex_line_${id}.html`));
+
+//     // res.render('apex/line/apex_line_2');
+//     // res.render('index');
+
+//     // res.write(html);
+//     // res.end(); 
+// });
+
+// app.get('/apex/area/:id', (req, res) => {
+//     const id = req.params.id;
+
+//     let filePath = `views/apex/area/apex_area_${id}.html`;
+//     console.log(`filePath: ${filePath}`);
+    
+//     res.sendFile(path.join(__dirname, filePath));
+//     // 1 area
+//     // 2 spline
+// });
+
+app.get('/apex/:tp/:id', (req, res) => {
+    const tp = req.params.tp;
+    const id = req.params.id;
+
+    console.log('hello');
+    let filePath = `views/apex/${tp}/apex_${tp}_${id}.html`;
+    console.log(`filePath: ${filePath}`);
+
+    res.sendFile(path.join(__dirname, filePath));
 });
