8e02e39866912bf943c185dd2bef9daf0132f500
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-04-13 17:20:29 +0900

    feat: v0.2.1 웹페이지 시안 업데이트, 라우트 경로 수정

--- server.js
+++ server.js
@@ -10,19 +10,20 @@ const port = 8080;
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
-app.get('/pages/home', (req, res) => {
-    // const data = {
-    //     title: 'My Page Title',
-    //     content: 'This is my page content.',
-    // };
-    // res.render('index', data);
+// const data = {
+//     title: 'My Page Title',
+//     content: 'This is my page content.',
+// };
+// res.render('index', data);
 
-    res.render('pages/index');
-});
+// app.get('/pages/home', (req, res) => {
 
-app.get('/pages/about', (req, res) => {
-    res.render('pages/about');
-});
+//     res.render('pages/home');
+// });
+
+// app.get('/pages/about', (req, res) => {
+//     res.render('pages/about');
+// });
 
 app.get('/css/:name.css', (req, res) => {
     const name = req.params.name;
@@ -31,32 +32,46 @@ app.get('/css/:name.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'css', `${name}.css`));
 });
 
-app.get('/:name.png', (req, res) => {
+app.get('/icons/:name.png', (req, res) => {
     const name = req.params.name;
 
     // console.log(path.join(__dirname, `icons/${name}.png`));
     res.sendFile(path.join(__dirname, `icons/${name}.png`));
 });
 
+app.get('/pages/:name', (req, res) => {
+
+    const name = req.params.name;
+
+    console.log(path.join(__dirname, `pages/${name}.ejs`));
+    // res.sendFile(path.join(__dirname, `pages/${name}.html`));
+    res.render(`pages/${name}`);
+});
+
 app.listen(port, function () {
     console.log('listening on ', port);
 });
 
-app.get('/', function (req, res) {
-    html = fs.readFileSync('samples/index.html');
-    res.writeHead(200);
-    // res.render('index');
-    res.write(html);
-    res.end();
-});
+// app.get('/pages/samples', function (req, res) {
+//     // html = fs.readFileSync('samples/index.html');
+//     // res.writeHead(200);
+//     // // res.render('index');
+//     // res.write(html);
+//     // res.end();
+//     res.render('pages/samples');
+// });
 
 app.get('/apex/:tp/:id', (req, res) => {
     const tp = req.params.tp;
     const id = req.params.id;
 
     // console.log('hello');
-    let filePath = `samples/apex/${tp}/apex_${tp}_${id}.html`;
-    console.log(`filePath: ${filePath}`);
+    // let filePath = `samples/apex/${tp}/apex_${tp}_${id}.html`;
+    // console.log(`filePath: ${filePath}`);
 
-    res.sendFile(path.join(__dirname, filePath));
+    // res.sendFile(path.join(__dirname, filePath));
+
+    console.log(path.join(__dirname, `samples/apex/${tp}/apex_${tp}_${id}.ejs`));
+
+    res.render(`samples/${tp}/apex_${tp}_${id}`);
 });
