d8c5f86925e2069395241e3f4f7e584a4166bc08
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-04-15 18:56:14 +0900

    0.2.8 feat: 1번 그래프 필터링 코드 추가해서 그래프 표시

--- server.js
+++ server.js
@@ -91,23 +91,158 @@ app.get('/data/NH/:id', (req, res) => {
     // res.send(key);
 });
 
+function _getData(id) {
+    // const id = req.params.id;
+    const bucketName = process.env.AWS_BUCKET_NAME;
+
+    let key = '';
+    switch (id) {
+        case '1':
+            key = process.env.AWS_BUCKET_KEY_1;
+            break;
+        case '2':
+            key = process.env.AWS_BUCKET_KEY_2;
+            break;
+        case '3':
+            key = process.env.AWS_BUCKET_KEY_3;
+            break;
+    }
+
+    const params = {
+        Bucket: bucketName,
+        Key: key
+    }
+
+    return new Promise((resolve, reject) => {
+        s3.getObject(params, (err, data) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                // console.log(data.Body.toString());
+                // _data = data.Body.toString();
+                // resolve(_data);
+                resolve(data.Body.toString());
+            }
+        });
+    });
+
+    // var _data = {};
+
+    // s3.getObject(params, (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    //     else {
+    //         // console.log(data.Body.toString());
+    //         _data = data.Body.toString();
+    //         // res.send(_data);
+    //         // console.log(_data);
+    //         return _data;
+    //     }
+    // });
+    // console.log(_data);
+}
+
 //#endregion S3
 
 //#region datas
 
 app.get('/data/1', (req, res) => {
-    const jsonData = {
-        "name": "John",
-        "age": 30,
-        address: {
-            city: 'New York',
-            state: 'NY',
-            country: 'USA'
-        }
-    };
+    // 111 s3 데이터를 받는다 (1번꺼)
+    _getData('1')
+        .then((data) => {
+            res.setHeader('Content-Type', 'application/json');
 
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(jsonData));
+            // console.log(data);
+            data = JSON.parse(data);
+
+            // ------------------------------
+
+            // 222 이걸 가공한다
+            const minmaxData = {};
+            const meanData = {};
+
+            // minmaxData, meanData 데이터 가공
+            for (const key in data) {
+                const value = data[key];
+                // console.log(value);
+                // console.log('(분류, name)품목-품종-등급 : ', `${value.PUMMOK}-${value.PUMJONG}-${value.DDD}`);
+                // console.log('(x축 값)날짜 : ', value.ADJ_DT);
+                // console.log('(y축 값)최소, 최대값 : ', [value.MIN, value.MAX]);
+                // console.log('(y축 값)평균값 : ', value.MEAN);
+                newKey = `${value.PUMMOK}-${value.PUMJONG}-${value.DDD}`;
+
+                // 최소, 최대값 정리
+                _minmaxData = {
+                    x: value.ADJ_DT,
+                    y: [value.MIN, value.MAX]
+                }
+
+                // 평균값 정리
+                _meanData = {
+                    x: value.ADJ_DT,
+                    y: value.MEAN
+                }
+
+                // 초기 할당 데이터의 경우 리스트 생성
+                if (minmaxData[newKey] === undefined) {
+                    minmaxData[newKey] = [];
+                }
+                if (meanData[newKey] === undefined) {
+                    meanData[newKey] = [];
+                }
+
+                // 데이터 추가
+                minmaxData[newKey].push(_minmaxData);
+                meanData[newKey].push(_meanData);
+            }
+            // console.log(newData);
+
+            // 333 최종 전달할 json 데이터 프레임 가져온다
+            const series = [];
+
+            // 444 거기 데이터 영역에 가공한 데이터 박는다
+            // 최소, 최대값 정리
+            for (const key in minmaxData) {
+                const value = minmaxData[key];
+                // console.log(key);   // key: (분류, name)품목-품종-등급 -> name
+                // console.log(value); // value: (x축 값)날짜, (y축 값)최소, 최대값 -> data
+                // console.log('---------------------');
+
+                series.push({
+                    type: 'rangeArea',
+                    name: key,
+                    data: value
+                });
+            }
+            // 평균값 정리
+            for (const key in meanData) {
+                const value = meanData[key];
+                // console.log(key);   // key: (분류, name)품목-품종-등급 -> name
+                // console.log(value); // value: (x축 값)날짜, (y축 값)평균값 -> data
+                // console.log('---------------------');
+
+                series.push({
+                    type: 'line',
+                    name: key,
+                    data: value
+                });
+            }
+            // console.log(series);
+
+            // ------------------------------
+
+            // console.log(typeof series)
+
+            // 5 전달
+            res.json(series);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
 });
 
 //#endregion datas
